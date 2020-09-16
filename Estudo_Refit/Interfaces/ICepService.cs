1118011ab1eb5ce681f23f1903f5875e09071a9a
Felipe Luz <extern.felipe.luz@t-systems.com.br>
2020-09-16 10:19:36 -0300

    Add project files.

---
+++ Estudo_Refit/Interfaces/ICepService.cs
@@ -0,0 +1,12 @@
+﻿using Estudo_Refit.Models;
+using Refit;
+using System.Threading.Tasks;
+
+namespace Estudo_Refit.Interfaces
+{
+    public interface ICepService
+    {
+        [Get("/ws/{cep}/json")]
+        Task<CepModel> GetAddressAsync(string cep);
+    }
+}
---
+++ Estudo_Refit/Program.cs
@@ -0,0 +1,28 @@
+﻿using Estudo_Refit.Interfaces;
+using Refit;
+using System;
+using System.Threading.Tasks;
+
+namespace Estudo_Refit
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var cepClient = RestService.For<ICepService>("https://viacep.com.br");
+            string meuCep = "09330806";
+            Console.WriteLine("Consultando dados do Cep:" + meuCep);
+
+            var address = await cepClient.GetAddressAsync(meuCep);
+
+            Console.Write($"" +
+                $"\nLocalidade:{address.Localidade}" +
+                $"\nUnidade:{address.Unidade}" +
+                $"\nLogradouro:{address.Logradouro}" +
+                $"\nBairro:{address.Bairro}" +
+                $"\nEstado:{address.Uf}" +
+                $"\nCódigo Ibge:{address.Ibge}");
+            Console.ReadKey();
+        }
+    }
+}
